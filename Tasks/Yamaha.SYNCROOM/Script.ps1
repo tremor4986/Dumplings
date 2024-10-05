bbd9e080fe9a948673e1548ed0b6e3113f0b36d2
AlexKoala <alexkoala@alexkoala.com>
2024-10-05 22:32:51 +0900

    Add: syncroom

---
+++ Tasks/Yamaha.SYNCROOM/Script.ps1
@@ -0,0 +1,25 @@
+$Object1 = Invoke-WebRequest -Uri 'https://syncroom.yamaha.com/global/v2/play/dl/' | ConvertFrom-Html
+
+# Version
+
+$this.CurrentState.Version = [regex]::Match(
+  $Object1.SelectSingleNode('/html/body/div/main/div/div[2]/div/div[1]/div/a[1]').Attributes['href'].Value, '(\d+\.\d+\.\d+)'
+  ).Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.SelectSingleNode('/html/body/div/main/div/div[2]/div/div[1]/div/a[1]').Attributes['href'].Value
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
