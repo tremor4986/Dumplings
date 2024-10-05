ca9d4e826692ce304d3a91bfd910ba7e60c1bac4
AlexKoala <alexkoala@alexkoala.com>
2024-10-06 00:17:14 +0900

    update

--- Tasks/NCSoft.Purple/Script.ps1
+++ Tasks/NCSoft.Purple/Script.ps1
@@ -1,15 +1,15 @@
-$Object1 = Invoke-WebRequest -Uri 'https://purple-store.plaync.com/api/conti/content?service=PurpleStore&alias=PurpleStore_Download' | ConvertFrom-Html
+$Object1 = Invoke-WebRequest -Uri 'https://purple-store.plaync.com/api/conti/content?service=PurpleStore&alias=PurpleStore_Download'
 
 # Version
 
 $this.CurrentState.Version = [regex]::Match(
-  $Object1.SelectSingleNode('/html/body/div/div[3]/a').Attributes['href'].Value, '(\d+\_\d+\_\d+\_\d+)'
+  $Object1.Content, '(\d+\_\d+\_\d+\_\d+)'
   ).Groups[1].Value.Replace('_', '.')
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   InstallerUrl ="https://gs-purple-inst.download.ncupdate.com/Purple/PurpleInstaller_" + [regex]::Match(
-  $Object1.SelectSingleNode('/html/body/div/div[3]/a').Attributes['href'].Value, '(\d+\_\d+\_\d+\_\d+)'
+  $Object1.Content, '(\d+\_\d+\_\d+\_\d+)'
   ).Groups[1].Value + ".exe"
 }
 
