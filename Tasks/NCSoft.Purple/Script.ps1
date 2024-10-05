c1679f23acc04b8b6111ea0a2d8bfbc9fe5582fc
AlexKoala <alexkoala@alexkoala.com>
2024-10-06 00:20:58 +0900

    update

--- Tasks/NCSoft.Purple/Script.ps1
+++ Tasks/NCSoft.Purple/Script.ps1
@@ -3,13 +3,13 @@ $Object1 = Invoke-WebRequest -Uri 'https://purple-store.plaync.com/api/conti/con
 # Version
 
 $this.CurrentState.Version = [regex]::Match(
-  $Object1.Content, '(\d+\_\d+\_\d+\_\d+)'
+  $Object1.Content, '(\d+_\d+_\d+_\d+)'
   ).Groups[1].Value.Replace('_', '.')
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   InstallerUrl ="https://gs-purple-inst.download.ncupdate.com/Purple/PurpleInstaller_" + [regex]::Match(
-  $Object1.Content, '(\d+\_\d+\_\d+\_\d+)'
+  $Object1.Content, '(\d+_\d+_\d+_\d+)'
   ).Groups[1].Value + ".exe"
 }
 
