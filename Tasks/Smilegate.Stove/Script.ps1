b3473b8904188526a805258df0747b740eb3434d
AlexKoala <alexkoala@alexkoala.com>
2024-10-05 17:11:28 +0900

    Add: stove

---
+++ Tasks/Smilegate.Stove/Script.ps1
@@ -0,0 +1,23 @@
+$Object1 = Invoke-RestMethod -Uri 'https://api.onstove.com/game/v1.0/stove/client/download'
+
+# Version
+
+$this.CurrentState.Version = "2.0.0." + [regex]::Match($Object1.value, '(^[0-9])'  ).Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = $Object1.value
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
