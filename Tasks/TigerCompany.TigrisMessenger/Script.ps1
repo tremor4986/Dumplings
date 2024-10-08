287a56f6e671ed51041bddc27cf13c03227dcc0b
AlexKoala <alexkoala@alexkoala.com>
2024-10-08 21:52:57 +0900

    update: tigris

---
+++ Tasks/TigerCompany.TigrisMessenger/Script.ps1
@@ -0,0 +1,29 @@
+$Object1 = Invoke-WebRequest -Uri 'https://www.tigrison.com/home' | ConvertFrom-Html
+
+# Version
+
+$this.CurrentState.Version = [regex]::Match(
+  $Object1.SelectSingleNode('/html/body/script[8]').Attributes['href'].Value, '(\d+\.\d+\.\d+)'
+  ).Groups[1].Value
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  InstallerUrl = 'https://www.tigrison.com/store/messenger/update/packages/' + [regex]::Match(
+  $Object1.SelectSingleNode('/html/body/script[8]').Attributes['href'].Value, '(\d+\.\d+\.\d+)'
+  ).Groups[1].Value + '/TigrisMessenger-' + [regex]::Match(
+  $Object1.SelectSingleNode('/html/body/script[8]').Attributes['href'].Value, '(\d+\.\d+\.\d+)'
+  ).Groups[1].Value + '%20Setup.exe'
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
