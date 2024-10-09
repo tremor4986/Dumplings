93128482c480377ef1d9a501a20d77919b41ac0a
AlexKoala <alexkoala@alexkoala.com>
2024-10-09 18:44:50 +0900

    Add: tortoisesvn

---
+++ Tasks/TortoiseSVN.TortoiseSVN/Script.ps1
@@ -0,0 +1,38 @@
+$Object1 = Invoke-WebRequest -Uri 'https://sourceforge.net/projects/tortoisesvn/files/' | ConvertFrom-Html
+$3Ver = [regex]::Match(
+  $Object1.SelectSingleNode('/html/body/div[5]/div[2]/div[1]/div/div[1]/article/section/div/div/div[2]/div/a[1]').Attributes['title'].Value, '(\d+\.\d+\.\d+)'
+  ).Groups[1].Value
+$4Ver = [regex]::Match(
+  $Object1.SelectSingleNode('/html/body/div[5]/div[2]/div[1]/div/div[1]/article/section/div/div/div[2]/div/a[1]').Attributes['title'].Value, '(\d+\.\d+\.\d+\.\d+)'
+  ).Groups[1].Value
+$InstUrl = 'https://sourceforge.net/projects/tortoisesvn/files/$($3Ver)/Application/TortoiseSVN-$($4Ver)-arch-svn-$($3Ver).msi/download'
+
+# Version
+$this.CurrentState.Version = $4Ver
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x86'
+  InstallerUrl = $InstUrl.Replace('arch', 'win32')
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $InstUrl.Replace('arch', 'x64')
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $InstUrl.Replace('arch', 'ARM64')
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Print()
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
