57c98f0c5907a1065ca8406debd5e5e99ab691f6
AlexKoala <alexkoala@alexkoala.com>
2024-10-09 19:44:55 +0900

    fix regex

--- Tasks/TortoiseSVN.TortoiseSVN/Script.ps1
+++ Tasks/TortoiseSVN.TortoiseSVN/Script.ps1
@@ -6,7 +6,7 @@ $4Ver = [regex]::Match(
   $Object1.SelectSingleNode('/html/body/div[4]/div[2]/div[1]/div/div[1]/article/section/div/div/div[2]/div/a[1]').Attributes['title'].Value, '(\d+\.\d+\.\d+\.\d+)'
   ).Groups[1].Value
 $ReleaseVer = [regex]::Match(
-  $Object1.SelectSingleNode('/html/body/div[4]/div[2]/div[1]/div/div[1]/article/section/div/div/div[2]/div/a[1]').Attributes['title'].Value, '(\d+\.\d+\.\d+\.\d+)'
+  $Object1.SelectSingleNode('/html/body/div[4]/div[2]/div[1]/div/div[1]/article/section/div/div/div[2]/div/a[1]').Attributes['title'].Value, '(\d+\.\d+\.\d+)'
   ).Groups[3].Value
 $InstUrl = 'https://sourceforge.net/projects/tortoisesvn/files/'+ $3Ver + '/Application/TortoiseSVN-' + $4Ver + '-arch-svn-' + $ReleaseVer + '.msi'
 
